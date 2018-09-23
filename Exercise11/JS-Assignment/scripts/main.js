8701b3548e85348248c70d79dee1a7b1510cdc14
npjordan <42592456+npjordan@users.noreply.github.com>
2018-09-22 22:42:05 -0500

    Update main.js

--- Exercise11/JS-Assignment/scripts/main.js
+++ Exercise11/JS-Assignment/scripts/main.js
@@ -5,10 +5,20 @@ function update() {
 
 
     ///Part 1
-    var firstName = getValue('firstName');
-    var lastName = getValue('lastName');
+    var firstName = "Nicholas";
+    var lastName = "Jordan";
     console.log(firstName);
     console.log(lastName);
+    var p1_fullName = firstName + " " + lastName;
+    console.log(p1_fullName)
+    if (firstName > 0){
+        var p1_valid = true}else{
+        var p1_valid = false};
+    if (lastName > 0){
+        var p1_valid = true}else{
+        var p1_valid = false};
+    var p1_fullNameLength = p1_fullName.length;
+    console.log(p1_fullNameLength);
 
 
 
