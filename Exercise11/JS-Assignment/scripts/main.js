307113519f459f3c8ead4296ee217d1ea54f9b94
npjordan <42592456+npjordan@users.noreply.github.com>
2018-09-24 13:56:24 -0500

    Update main.js

--- Exercise11/JS-Assignment/scripts/main.js
+++ Exercise11/JS-Assignment/scripts/main.js
@@ -10,35 +10,47 @@ function update() {
     console.log(firstName);
     console.log(lastName);
     var p1_fullName = firstName + " " + lastName;
-    console.log(p1_fullName)
+    console.log(p1_fullName);
+    document.getElementById("p1_fullName").innerHTML = p1_fullName;
     if (firstName.indexOf('Nicholas')){
         var p1_valid = true}else{
         var p1_valid = false};
     if (lastName.indexOf('Jordan')){
         var p1_valid = true}else{
         var p1_valid = false};
+    console.log(p1_valid);
+    document.getElementById("partOne").innerHTML = p1_valid;
     var p1_fullNameLength = p1_fullName.length;
     console.log(p1_fullNameLength);
+    document.getElementById("p1_fullNameLength").innerHTML = p1_fullNameLength;
 
 
 
 
     // Part 2
-    var emailAddress = "nicholas.jordan.10@gmail.com";
-    console.log(emailAddress);
+    var p2_email = "nicholas.jordan.10@gmail.com";
+    console.log(p2_email);
+    document.getElementById("p2_email").innerHTML = p2_email;
     var p2_valid = (email.indexOf('@') > -1);
     console.log(p2_valid);
+    document.getElementById("p2_valid").innerHTML = p2_valid;
     var p2_emailLength = emailAddress.length;
     console.log(emailLength);
+    document.getElementById("p2_emailLength").innerHTML = p2_emailLength;
 
     // Part 3
     var randomNumber = getValue('randomNumber');
     console.log(randomNumber);
     var p3_number = randomNumber;
     console.log(p3_number);
-    if (isNan(p3_number) = false){p3_number = true
-    }else{p3_number = false
+    document.getElementById("p3_number").innerHTML = p3_number;
+    p3_valid = true;
+    p3_valid = false;
+    if (isNan(p3_number) = false){p3_valid = true
+    }else{p3_valid = false
     };
+    console.log(p3_valid);
+    document.getElementById("p3_valid").innerHTML = p3_valid;
 
 
 
@@ -50,6 +62,8 @@ function update() {
     console.log(p4_arrayLength);
     var p4_valid = return.arrayItems;
     console.log(p4_valid);
+    document.getElementById("p4_arrayLength").innerHTML = p4_arrayLength;
+    document.getElementById("p4_valid").innerHTML = p4_valid;
     
     
 
